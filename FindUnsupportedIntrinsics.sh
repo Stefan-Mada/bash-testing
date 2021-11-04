cb5967e02d146c756dae6ac294e36f1825f1571f
Stefan-Mada <Stefan.Mada@utah.edu>
2021-11-03 20:54:47 -0600

    Made faster

--- FindUnsupportedIntrinsics.sh
+++ FindUnsupportedIntrinsics.sh
@@ -3,7 +3,10 @@
 launchParameter="$HOME/alive2-x86/build/alivecc"
 filePath="vpx_dsp/x86"
 
-export ALIVECC_SMT_TO=10
+export ALIVECC_SMT_TO=5
+export ALIVECC_SUBPROCESS_TIMEOUT=1
+export ALIVECC_DISABLE_UNDEF_INPUT=1
+export ALIVECC_PARALLEL_UNRESTRICTED=1
 
 #grep -hnr "Unknown libcall" $HOME/alivecc.txt > output.txt
 grep -hnr "\-o $filePath" $HOME/compile.txt > output.txt #finds lines in target folder
